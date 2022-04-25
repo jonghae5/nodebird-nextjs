eb06f3917a5a326dd5b98b9a59070769d4aa0714
jonghae5 <dhwhdgo2368@gmail.com>
2022-04-25 21:28:44 +0900

    BackUrl

--- components/PostForm.js
+++ components/PostForm.js
@@ -8,6 +8,7 @@ import {
   UPLOAD_IMAGES_REQUEST,
   REMOVE_IMAGE,
 } from '../reducers/post';
+import { backUrl } from '../config/config';
 
 const PostForm = () => {
   const { imagePaths, addPostDone } = useSelector(state => state.post);
@@ -86,11 +87,7 @@ const PostForm = () => {
         {imagePaths.map((v, i) => {
           return (
             <div key={v} style={{ display: 'inline-block' }}>
-              <img
-                src={`http://localhost:3065/${v}`}
-                style={{ width: '200px' }}
-                alt={v}
-              />
+              <img src={`${backUrl}/${v}`} style={{ width: '200px' }} alt={v} />
               <div>
                 <Button onClick={onRemoveImage(i)}>제거</Button>
               </div>
