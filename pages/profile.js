10805a72506e36f1bbb6e6cf812316abf88d643e
jonghae5 <dhwhdgo2368@gmail.com>
2022-04-27 22:10:59 +0900

    Profile BackUrl

--- pages/profile.js
+++ pages/profile.js
@@ -15,6 +15,7 @@ import {
   LOAD_FOLLOWINGS_REQUEST,
   LOAD_MY_INFO_REQUEST,
 } from '../reducers/user';
+import { backUrl } from '../config/config';
 
 const fetcher = url =>
   axios.get(url, { withCredentials: true }).then(result => result.data);
@@ -24,11 +25,11 @@ const Profile = () => {
   const [followersLimit, setFollowersLimit] = useState(3);
 
   const { data: followingsData, error: followingError } = useSWR(
-    `http://localhost:3065/user/followings?limit=${followingsLimit}`,
+    `${backUrl}/user/followings?limit=${followingsLimit}`,
     fetcher
   );
   const { data: followersData, error: followerError } = useSWR(
-    `http://localhost:3065/user/followers?limit=${followersLimit}`,
+    `${backUrl}/user/followers?limit=${followersLimit}`,
     fetcher
   );
   const { me } = useSelector(state => state.user);
